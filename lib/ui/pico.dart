e87c2023893a57538fd841784c769297c43111ff
Emil <emil.bergold@outlook.com>
2015-04-10 15:50:29 +0200

    add events to picocard

--- lib/ui/pico.dart
+++ lib/ui/pico.dart
@@ -15,8 +15,24 @@ class PicoCard extends ListItemCard with TemplateInjector {
   StreamController _onClickStartCtrl = new StreamController.broadcast();
   Stream get onClickStart => _onClickStartCtrl.stream;
   
+  StreamController _onClickStopCtrl = new StreamController.broadcast();
+  Stream get onClickStop => _onClickStopCtrl.stream;
+  
+  StreamController _onClickOpenCtrl = new StreamController.broadcast();
+  Stream get onClickOpen => _onClickOpenCtrl.stream;
+  
+  StreamController _onClickClearCtrl = new StreamController.broadcast();
+  Stream get onClickClear => _onClickClearCtrl.stream;
+  
+  StreamController _onClickDeleteCtrl = new StreamController.broadcast();
+  Stream get onClickDelete => _onClickDeleteCtrl.stream;
+  
   PicoCard(TemplateElement tpl) : super(tpl) {
     template.querySelector('.pico-btn-start').onClick.pipe(_onClickStartCtrl);
+    template.querySelector('.pico-btn-stop').onClick.pipe(_onClickStopCtrl);
+    template.querySelector('.pico-btn-open').onClick.pipe(_onClickOpenCtrl);
+    template.querySelector('.pico-btn-clear').onClick.pipe(_onClickClearCtrl);
+    template.querySelector('.pico-btn-delete').onClick.pipe(_onClickDeleteCtrl);
   }
   
 }
